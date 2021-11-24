c522329c9bfdef02652a12026d9baf1d8ba7884c
polianskyi <polyanskiy.d.v@gmail.com>
2021-11-24 23:32:39 +0200

    add some info to output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,21 @@
 set -eu
 
 echo 'START'
-exit 0
 
 TEMP_SSH_PRIVATE_KEY_FILE='../id_rsa_github'
 TEMP_SFTP_FILE='../sftp'
 
 # keep string format
-# printf "%s" "$4" > $TEMP_SSH_PRIVATE_KEY_FILE
+printf "%s" "$4" > $TEMP_SSH_PRIVATE_KEY_FILE
 # avoid Permissions too open
-# chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
+chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 
-# stat $TEMP_SSH_PRIVATE_KEY_FILE
+stat $TEMP_SSH_PRIVATE_KEY_FILE
 
 echo 'ssh start'
 
-# ssh -o StrictHostKeyChecking=no -p ${3} -i ${TEMP_SSH_PRIVATE_KEY_FILE} $1@$2 mkdir -p $6
+ssh -o StrictHostKeyChecking=no -p ${3} -i ${TEMP_SSH_PRIVATE_KEY_FILE} $1@$2 mkdir -p $6
+exit 0
 
 echo 'sftp start'
 # create a temporary file containing sftp commands

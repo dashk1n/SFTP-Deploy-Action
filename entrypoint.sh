bc18bb86953f4e05e0cf034f808e2a5a631af6f7
polianskyi <polyanskiy.d.v@gmail.com>
2021-11-25 11:07:47 +0200

    add some info to output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,7 @@
 #set -e at the top of your script will make the script exit with an error whenever an error occurs (and is not explicitly handled)
 set -eu
 
-echo 'START'
-
-TEMP_SSH_PRIVATE_KEY_FILE='../id_rsa_github'
+TEMP_SSH_PRIVATE_KEY_FILE='../id_rsa_private_key'
 TEMP_SFTP_FILE='../sftp'
 
 # keep string format
@@ -17,7 +15,7 @@ stat $TEMP_SSH_PRIVATE_KEY_FILE
 
 echo 'ssh start'
 
-ssh -o StrictHostKeyChecking=no -p ${3} -i ${TEMP_SSH_PRIVATE_KEY_FILE} $1@$2 mkdir -p $6
+ssh -o StrictHostKeyChecking=no -p $3 -i ${TEMP_SSH_PRIVATE_KEY_FILE} $1@$2 mkdir -p $6
 
 echo 'sftp start'
 # create a temporary file containing sftp commands
